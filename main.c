2d7bde8c15a4845d2421bc02f5ea7fe2ea27dbed
Théo Porzio <theo.porzio@student.yncrea.fr>
2021-05-17 10:21:02 +0200

    First commit, initialization

---
+++ main.c
@@ -0,0 +1,12 @@
+//
+// Created by StillNix on 17/05/2021.
+//
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Hello World !");
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by StillNix on 17/05/2021.
+//
+
+#ifndef PROTOKODE_MAIN_H
+#define PROTOKODE_MAIN_H
+
+#endif //PROTOKODE_MAIN_H
