b3e9baa2c1acd4e0b1e83af42955c477557ab181
corentin <corentin.sotoca@student.yncrea.fr>
2021-05-27 10:59:54 +0200

    Auto stash before merge of "main" and "origin/main"
    add json fonctionnalities to contact

--- Web/routes/signup.js
+++ Web/routes/signup.js
@@ -1,6 +1,8 @@
+const fs = require('fs');
 const express = require('express');
-var bodyParser = require('body-parser');
 const app = express();
+let bodyParser = require('body-parser');
+
 
 app.use(bodyParser.json())
 
@@ -9,9 +11,13 @@ app.post('/', function(req, res, next) {
         title: 'Sign Up',
         name: 'Test'
     });
+
     console.log(req.body.email);
     console.log(req.body.name);
     console.log(req.body.message);
+
+    let data = JSON.parse(fs.readFileSync("public/data/feedback.json"));
+    console.log(data);
 })
 
 module.exports = app;
--- Web/routes/test.js
+++ Web/routes/test.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const map = require('../map.json');
+const map = require('../public/data/map.json');
 
 router.get('/', function(req, res, next) {
     res.render('test', {
